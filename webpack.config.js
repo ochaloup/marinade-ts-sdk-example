4d8106cbf65e99e0f57392daee67f20131d13106
Ondra Chaloupka <chalda@marinade.finance>
2023-05-25 12:24:49 +0200

    example ts webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+var config = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+    },
+  },
+  plugins: [new NodePolyfillPlugin()],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  experiments: {
+    outputModule: true,
+  },
+  target: 'es6',
+  output : {
+    filename: 'marinade-ts-sdk-example.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: 'module',
+    },
+    chunkFormat: 'module',
+		module: true,
+  },
+  devtool: 'source-map',
+}
+
+module.exports = [config]
